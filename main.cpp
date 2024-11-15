d021b6c66a54b95a958afd4222742010924de77a
Kai-Cheng Mei <kennymei47@gmail.com>
2024-11-15 08:32:13 -0800

    update3

--- main.cpp
+++ main.cpp
@@ -5,13 +5,37 @@
 #include <iostream>
 using namespace std;
 
-void display();
+// print 
+void display(const deque<Car> queue) {
+    cout << "Queue:" << endl;
+    for (const auto car : queue) {
+        car.print();
+    }
+}
 
 int main() {
-    deque<Car> queue;
-    for(int i = 0;i < 2; i++) {
-    queue.push_back(Car());
-    }
+    deque<Car> Cqueue;
 
+    // initial 
+    for(int i = 0;i < 2; i++) {
+    Cqueue.push_back(Car());
+    }
     cout << "Initial queue:" << endl;
+    display(Cqueue);
+
+    int time=0;
+    while(!Cqueue.empty()) {
+        cout << "Time " << time << "Operation: ";
+        // 55% probability that the car at the head of the line pays its toll and leaves the toll booth
+
+        if (rand() % 100 < 55) {
+            cout << "Car paid: ";
+            Cqueue.front().print();
+            Cqueue.pop_front();
+        } 
+        // 45% probability that another car joins the line for the toll booth
+        else {
+
+        }
+    }
 }
