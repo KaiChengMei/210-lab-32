cf4d66294d6457deaa408aea8bdde3869101cc9a
Kai-Cheng Mei <kennymei47@gmail.com>
2024-11-17 15:31:47 -0800

    update6

--- main.cpp
+++ main.cpp
@@ -44,36 +44,49 @@ int main() {
     cout << endl;
 
     for( int time = 1; time <= endtime; time++) {
+        cout << endl;
         cout << "Time " << time << endl;
 
         for ( int a = 0; a < lanecount; a++) {
-            int random = rand() % 100;
+            auto& lane = lanes[a];
 
-            // 46% probability that the car at the head of the queue pays its toll and leaves the queue
-            if (random < pay) {
-                cout << "Lane " << a+1 << " paid: ";
-                lanes[a].front().print();
-                lanes[a].pop_front();
-            } 
-            // 39% probability that another car joins the queue
-            else if (random < pay+join ){
-                Car newCar;
-                cout << "Lane " << a+1 << " lane: ";
-                newCar.print();
-                lanes[a].push_back(newCar);
-            }
-            // 15% probability that the rear car will shift lanes
-            else if (random < pay+join+switchs ) {
-                // choose the new lane except the origin lane
-                int newlane = 0;
-                while (newlane = (a+1)) {
-                    newlane = rand() % lanecount;
+            if (!lane.empty()) {
+                int random = rand() % 100;
+
+                // 46% probability that the car at the head of the queue pays its toll and leaves the queue
+                if (random < pay) {
+                    cout << "Lane " << a+1 << " paid: ";
+                    lane.front().print();
+                    lane.pop_front();
+                } 
+                // 39% probability that another car joins the queue
+                else if (random < pay+join ){
+                    Car newCar;
+                    cout << "Lane " << a+1 << " joined: ";
+                    newCar.print();
+                    lane.push_back(newCar);
                 }
+                // 15% probability that the rear car will shift lanes
+                else if (random < pay+join+switchs ) {
+                    // choose the new lane except the origin lane
+                    int newlane = 0;
+                    while (newlane == (a+1)) {
+                        newlane = rand() % lanecount;
+                    }
 
-                cout << "Lane " << (a+1) << " switched: ";
-                lanes[a].back.print();
-                lanes[newlane].push_back(lanes[a].back());
-                lanes[a].pop_back();
+                    cout << "Lane " << (a+1) << " switched: ";
+                    lane.back().print();
+                    lanes[newlane].push_back(lane.back());
+                    lane.pop_back();
+                }
+            }
+
+            // if lane empty 50/50 a new car enters the queue or not
+            else if (rand() % 2 == 0) {
+                Car newCar;
+                cout << "Lane " << a+1 << " joined: ";
+                newCar.print();
+                lane.push_back(newCar);
             }
 
         }
